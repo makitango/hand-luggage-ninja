5f5dd25b80a15cf40538443ac626807d11f59ff5
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-07-10 23:39:51 +0200

    add title

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ export default function HomePage({ bags }) {
   console.log("pages/index.js", bags);
   return (
     <div>
+      <h1>Hand luggage ninja</h1>
       <List bags={bags} />
     </div>
   );
