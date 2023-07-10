4454295f9d1d1d6a5ff49ce5500deaad54537bf8
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-07-11 01:13:48 +0200

    remove metric/imperial headlines

--- pages/List/index.js
+++ pages/List/index.js
@@ -6,24 +6,20 @@ export default function List({ bags }) {
           <li key={bag.slug}>
             <h2>{bag.slug}</h2>
             <h3>Personal Item</h3>
-            <h4>Metric</h4>
             <p>
               {bag.personalItem.metric.length}x{bag.personalItem.metric.width}x
               {bag.personalItem.metric.height} cm
             </p>
-            <h4>Imperial</h4>
             <p>
               {bag.personalItem.imperial.length}x
               {bag.personalItem.imperial.width}x
               {bag.personalItem.imperial.height} in
             </p>
             <h3>Cabin bag</h3>
-            <h4>Metric</h4>
             <p>
               {bag.cabinBag.metric.length}x{bag.cabinBag.metric.width}x
               {bag.cabinBag.metric.height} cm
             </p>
-            <h4>Imperial</h4>
             <p>
               {bag.cabinBag.imperial.length}x{bag.cabinBag.imperial.width}x
               {bag.cabinBag.imperial.height} in
