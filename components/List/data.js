ee7c1f4084463582e5a0995b382c5f43773759e4
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-07-10 23:28:04 +0200

    redo array with bags, redo List

--- components/List/data.js
+++ components/List/data.js
@@ -1,76 +1,467 @@
 export const bags = [
+  //   {
+  //     slug: "British Airways",
+  //     personalItem: {
+  //       length: 40,
+  //       width: 30,
+  //       height: 15,
+  //     },
+  //     cabinBag: {
+  //       length: 56,
+  //       width: 45,
+  //       height: 25,
+  //       isIncluded: true,
+  //     },
+  //   },
+
+  //   {
+  //     slug: "EasyJet",
+  //     personalItem: {
+  //       length: 45,
+  //       width: 36,
+  //       height: 20,
+  //     },
+  //     cabinBag: {
+  //       length: 56,
+  //       width: 45,
+  //       height: 25,
+  //       isIncluded: false,
+  //     },
+  //   },
+
+  //   {
+  //     slug: "Eurowings",
+  //     personalItem: {
+  //       length: 40,
+  //       width: 30,
+  //       height: 25,
+  //     },
+  //     cabinBag: {
+  //       length: 55,
+  //       width: 40,
+  //       height: 23,
+  //       isIncluded: true,
+  //     },
+  //   },
+  //////////////////////////////////////////////////////////////////////////
+  {
+    slug: "Air Canada",
+    personalItem: {
+      metric: { length: 43, width: 33, height: 16 },
+      imperial: { length: 17, width: 13, height: 6 },
+    },
+    cabinBag: {
+      metric: { length: 55, width: 40, height: 23 },
+      imperial: { length: 21.5, width: 15.5, height: 9 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "Air Europa Líneas Aéreas",
+    personalItem: {
+      metric: { length: 20, width: 30, height: 35 },
+      imperial: { length: 7.9, width: 11.8, height: 13.8 },
+    },
+    cabinBag: {
+      metric: { length: 55, width: 35, height: 25 },
+      imperial: { length: 21.7, width: 13.8, height: 9.9 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "Air France-KLM",
+    personalItem: {
+      metric: { length: 40, width: 30, height: 15 },
+      imperial: { length: 16, width: 12, height: 6 },
+    },
+    cabinBag: {
+      metric: { length: 55, width: 35, height: 25 },
+      imperial: { length: 21.7, width: 13.8, height: 9.9 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "Aer Lingus",
+    personalItem: {
+      metric: { length: 25, width: 33, height: 20 },
+      imperial: { length: 10, width: 13, height: 8 },
+    },
+    cabinBag: {
+      metric: { length: 55, width: 40, height: 24 },
+      imperial: { length: 21.5, width: 15.5, height: 9.5 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "Allegiant Air",
+    personalItem: {
+      metric: { length: 45.72, width: 35.56, height: 20.32 },
+      imperial: { length: 18, width: 14, height: 8 },
+    },
+    cabinBag: {
+      metric: { length: 55.88, width: 40.64, height: 25.4 },
+      imperial: { length: 22, width: 16, height: 10 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "American Airlines",
+    personalItem: {
+      metric: { length: 45, width: 35, height: 20 },
+      imperial: { length: 18, width: 14, height: 8 },
+    },
+    cabinBag: {
+      metric: { length: 56, width: 36, height: 23 },
+      imperial: { length: 22, width: 14, height: 9 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "Austrian Airlines",
+    personalItem: {
+      metric: { length: 40, width: 30, height: 10 },
+      imperial: { length: 15.7, width: 11.8, height: 3.9 },
+    },
+    cabinBag: {
+      metric: { length: 55, width: 40, height: 23 },
+      imperial: { length: 21.6, width: 15.7, height: 9 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "Avianca & TACA",
+    personalItem: {
+      metric: { length: 45.7, width: 35.6, height: 25.4 },
+      imperial: { length: 18, width: 14, height: 10 },
+    },
+    cabinBag: {
+      metric: { length: 55.9, width: 35.6, height: 25.4 },
+      imperial: { length: 22, width: 14, height: 10 },
+      isIncluded: true,
+    },
+  },
   {
     slug: "British Airways",
     personalItem: {
-      height: 40,
-      width: 30,
-      depth: 15,
+      metric: { length: 40, width: 30, height: 15 },
+      imperial: { length: 16, width: 12, height: 6 },
     },
     cabinBag: {
-      height: 56,
-      width: 45,
-      depth: 25,
+      metric: { length: 56, width: 45, height: 25 },
+      imperial: { length: 22, width: 18, height: 10 },
       isIncluded: true,
     },
   },
-
   {
-    slug: "EasyJet",
+    slug: "Brussels Airlines",
     personalItem: {
-      height: 45,
-      width: 36,
-      depth: 20,
+      metric: { length: 40, width: 30, height: 10 },
+      imperial: { length: 15.7, width: 11.8, height: 3.9 },
     },
     cabinBag: {
-      height: 56,
-      width: 45,
-      depth: 25,
-      isIncluded: false,
+      metric: { length: 55, width: 40, height: 23 },
+      imperial: { length: 21.6, width: 15.7, height: 9 },
+      isIncluded: true,
     },
   },
-
+  {
+    slug: "Caribbean Airlines",
+    personalItem: {
+      metric: { length: 43, width: 33, height: 15 },
+      imperial: { length: 17, width: 13, height: 6 },
+    },
+    cabinBag: {
+      metric: { length: 56, width: 35, height: 23 },
+      imperial: { length: 22, width: 14, height: 9 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "Cathay Pacific Airways",
+    personalItem: {
+      metric: { length: 40, width: 30, height: 15 },
+      imperial: { length: 16, width: 12, height: 6 },
+    },
+    cabinBag: {
+      metric: { length: 56, width: 36, height: 23 },
+      imperial: { length: 22, width: 14, height: 9 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "Copa Airlines",
+    personalItem: {
+      metric: { length: 43, width: 25, height: 22 },
+      imperial: { length: 17, width: 10, height: 9 },
+    },
+    cabinBag: {
+      metric: { length: 56, width: 36, height: 26 },
+      imperial: { length: 22, width: 14, height: 10 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "EasyJet Airline",
+    personalItem: {
+      metric: { length: 45, width: 36, height: 20 },
+      imperial: { length: 17.7, width: 14.2, height: 7.9 },
+    },
+    cabinBag: {
+      metric: { length: 56, width: 45, height: 25 },
+      imperial: { length: 22, width: 17.7, height: 9.8 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "El Al Israel Airlines",
+    personalItem: {
+      metric: { length: 38, width: 30, height: 18 },
+      imperial: { length: 15, width: 11.8, height: 7 },
+    },
+    cabinBag: {
+      metric: { length: 56, width: 45, height: 25 },
+      imperial: { length: 22, width: 17.7, height: 9.8 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "Etihad Airways",
+    personalItem: {
+      metric: { length: 39, width: 23, height: 19 },
+      imperial: { length: 15.3, width: 9, height: 7.5 },
+    },
+    cabinBag: {
+      metric: { length: 56, width: 36, height: 23 },
+      imperial: { length: 22, width: 14.2, height: 9 },
+      isIncluded: true,
+    },
+  },
   {
     slug: "Eurowings",
     personalItem: {
-      height: 40,
-      width: 30,
-      depth: 25,
+      metric: { length: 40, width: 30, height: 25 },
+      imperial: { length: 15.7, width: 11.8, height: 9.8 },
     },
     cabinBag: {
-      height: 55,
-      width: 40,
-      depth: 23,
+      metric: { length: 55, width: 40, height: 23 },
+      imperial: { length: 21.7, width: 15.7, height: 9 },
       isIncluded: true,
     },
   },
-
+  {
+    slug: "EVA Air",
+    personalItem: {
+      metric: { length: 40, width: 30, height: 10 },
+      imperial: { length: 16, width: 12, height: 4 },
+    },
+    cabinBag: {
+      metric: { length: 56, width: 36, height: 23 },
+      imperial: { length: 22, width: 14, height: 9 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "Finnair",
+    personalItem: {
+      metric: { length: 40, width: 30, height: 15 },
+      imperial: { length: 16, width: 12, height: 6 },
+    },
+    cabinBag: {
+      metric: { length: 55, width: 40, height: 23 },
+      imperial: { length: 21.6, width: 15.7, height: 9 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "Frontier Airlines",
+    personalItem: {
+      metric: { length: 45.72, width: 35.56, height: 20.32 },
+      imperial: { length: 18, width: 14, height: 8 },
+    },
+    cabinBag: {
+      metric: { length: 60.96, width: 40.64, height: 25.4 },
+      imperial: { length: 24, width: 16, height: 10 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "Iberia",
+    personalItem: {
+      metric: { length: 40, width: 30, height: 15 },
+      imperial: { length: 15.7, width: 11.8, height: 5.9 },
+    },
+    cabinBag: {
+      metric: { length: 56, width: 40, height: 25 },
+      imperial: { length: 22, width: 15.7, height: 9.8 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "ITA Airways",
+    personalItem: {
+      metric: { length: 45, width: 36, height: 20 },
+      imperial: { length: 17.7, width: 14.2, height: 7.9 },
+    },
+    cabinBag: {
+      metric: { length: 55, width: 35, height: 25 },
+      imperial: { length: 21.6, width: 13.8, height: 9.8 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "JetBlue Airways",
+    personalItem: {
+      metric: { length: 43.2, width: 33, height: 20.32 },
+      imperial: { length: 17, width: 13, height: 8 },
+    },
+    cabinBag: {
+      metric: { length: 55.88, width: 35.56, height: 22.86 },
+      imperial: { length: 22, width: 14, height: 9 },
+      isIncluded: true,
+    },
+  },
   {
     slug: "Lufthansa",
     personalItem: {
-      height: 40,
-      width: 30,
-      depth: 10,
+      metric: { length: 40, width: 30, height: 10 },
+      imperial: { length: 15.7, width: 11.8, height: 3.9 },
     },
     cabinBag: {
-      height: 55,
-      width: 40,
-      depth: 23,
+      metric: { length: 55, width: 40, height: 23 },
+      imperial: { length: 21.6, width: 15.7, height: 9 },
       isIncluded: true,
     },
   },
-
   {
-    slug: "Ryanair",
+    slug: "Norwegian Air Shuttle",
     personalItem: {
-      height: 40,
-      width: 25,
-      depth: 20,
+      metric: { length: 38, width: 30, height: 20 },
+      imperial: { length: 15, width: 11.8, height: 7.9 },
     },
     cabinBag: {
-      height: 55,
-      width: 40,
-      depth: 20,
+      metric: { length: 55, width: 40, height: 23 },
+      imperial: { length: 21.6, width: 15.7, height: 9.1 },
       isIncluded: false,
+    },
+  },
+  {
+    slug: "RyanAir",
+    personalItem: {
+      metric: { length: 40, width: 25, height: 20 },
+      imperial: { length: 15.7, width: 9.8, height: 7.9 },
+    },
+    cabinBag: {
+      metric: { length: 55, width: 40, height: 20 },
+      imperial: { length: 21.7, width: 15.7, height: 7.9 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "Scandinavian Airlines (SAS)",
+    personalItem: {
+      metric: { length: 40, width: 30, height: 15 },
+      imperial: { length: 15.7, width: 11.8, height: 5.9 },
+    },
+    cabinBag: {
+      metric: { length: 55, width: 40, height: 23 },
+      imperial: { length: 21.7, width: 15.7, height: 9 },
+      isIncluded: false,
+    },
+  },
+  {
+    slug: "Southwest Airlines",
+    personalItem: {
+      metric: { length: 41, width: 34.3, height: 22 },
+      imperial: { length: 16.25, width: 13.5, height: 8 },
+    },
+    cabinBag: {
+      metric: { length: 60.9, width: 40.6, height: 25.4 },
+      imperial: { length: 24, width: 16, height: 10 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "Sunwing Airlines",
+    personalItem: {
+      metric: { length: 28, width: 23, height: 13 },
+      imperial: { length: 11, width: 9, height: 5 },
+    },
+    cabinBag: {
+      metric: { length: 51, width: 40, height: 23 },
+      imperial: { length: 20, width: 16, height: 9 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "Swiss International Air Lines",
+    personalItem: {
+      metric: { length: 40, width: 30, height: 10 },
+      imperial: { length: 15.7, width: 11.8, height: 3.9 },
+    },
+    cabinBag: {
+      metric: { length: 55, width: 40, height: 23 },
+      imperial: { length: 21.7, width: 15.7, height: 9 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "Thai Airways",
+    personalItem: {
+      metric: { length: 37.5, width: 25, height: 12.5 },
+      imperial: { length: 15, width: 10, height: 5 },
+    },
+    cabinBag: {
+      metric: { length: 56, width: 45, height: 25 },
+      imperial: { length: 22, width: 18, height: 10 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "Turkish Airlines",
+    personalItem: {
+      metric: { length: 40, width: 30, height: 15 },
+      imperial: { length: 15.7, width: 11.8, height: 5.9 },
+    },
+    cabinBag: {
+      metric: { length: 55, width: 40, height: 23 },
+      imperial: { length: 21.6, width: 15.7, height: 9 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "United Airlines",
+    personalItem: {
+      metric: { length: 43, width: 25, height: 22 },
+      imperial: { length: 17, width: 10, height: 9 },
+    },
+    cabinBag: {
+      metric: { length: 56, width: 35, height: 22 },
+      imperial: { length: 22, width: 14, height: 9 },
+      isIncluded: true,
+    },
+  },
+  {
+    slug: "VIVA Aerobus",
+    personalItem: {
+      metric: { length: 45, width: 35, height: 20 },
+      imperial: { length: 18, width: 14, height: 8 },
+    },
+    cabinBag: {
+      metric: { length: 55, width: 40, height: 25 },
+      imperial: { length: 22, width: 16, height: 10 },
+      isIncluded: false,
+    },
+  },
+  {
+    slug: "WestJet",
+    personalItem: {
+      metric: { length: 41, width: 33, height: 15 },
+      imperial: { length: 16, width: 13, height: 6 },
+    },
+    cabinBag: {
+      metric: { length: 53, width: 38, height: 23 },
+      imperial: { length: 21, width: 15, height: 9 },
+      isIncluded: true,
     },
   },
 ];
--- pages/List/index.js
+++ pages/List/index.js
@@ -1,20 +1,31 @@
 export default function List({ bags }) {
+  console.log(bags);
   return (
     <ul>
       {bags.map((bag) => {
+        console.log({ bags });
+
         return (
           <li key={bag.slug}>
-            {bag.slug.value}
-            <li>
-              Personal item height={bag.personalItem.height.value}
-              width={bag.personalItem.width.value}
-              depth={bag.personalItem.depth.value}
-            </li>
-            <li>
-              Cabin bag height={bag.cabinBag.height.value}
-              width={bag.cabinBag.width.value}
-              depth={bag.cabinBag.depth.value}
-            </li>
+            <h2>{bag.slug}</h2>
+            <h3>Personal Item</h3>
+            <h4>Metric</h4>
+            <p>Length {bag.personalItem.metric.length} cm</p>
+            <p>Width {bag.personalItem.metric.width} cm</p>
+            <p>Height {bag.personalItem.metric.height} cm</p>
+            <h4>Imperial</h4>
+            <p>Length {bag.personalItem.imperial.length} in</p>
+            <p>Width {bag.personalItem.imperial.width} in</p>
+            <p>Height {bag.personalItem.imperial.height} in</p>
+            <h3>Cabin bag</h3>
+            <h4>Metric</h4>
+            <p>Length {bag.cabinBag.metric.length} cm</p>
+            <p>Width {bag.cabinBag.metric.width} cm</p>
+            <p>Height {bag.cabinBag.metric.height} cm</p>
+            <h4>Imperial</h4>
+            <p>Length {bag.cabinBag.imperial.length} in</p>
+            <p>Width {bag.cabinBag.imperial.width} in</p>
+            <p>Height {bag.cabinBag.imperial.height} in</p>
           </li>
         );
       })}
