2a747d2407e01140569968d81cf439f418c99110
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-07-11 18:49:41 +0200

    add sort buttons, add keys to data.js, add handle functions for buttons

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,66 @@
 import GlobalStyle from "../styles";
-import { bags } from "../components/List/data";
-import List from "./List";
+import { bags } from "../lib/data";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [sortedBags, setSortedBags] = useState(bags);
+
+  function handleSortBagsAlphabetical() {
+    setSortedBags(bags);
+  }
+
+  function handleSortBagsPersonalItem() {
+    const sortedBags = bags
+      .map(({ slug, personalItem, cabinBag }) => {
+        const personalItemVolume = (
+          (personalItem.metric.length *
+            personalItem.metric.width *
+            personalItem.metric.height) /
+          1000
+        ).toFixed(1);
+
+        return {
+          slug,
+          personalItem,
+          cabinBag,
+          personalItemVolume: parseFloat(personalItemVolume),
+        };
+      })
+      .sort((a, b) => b.personalItemVolume - a.personalItemVolume);
+    setSortedBags(sortedBags);
+  }
+
+  function handleSortBagsCabinBag() {
+    const sortedBags = bags
+      .map(({ slug, personalItem, cabinBag }) => {
+        const cabinBagVolume = (
+          (cabinBag.metric.length *
+            cabinBag.metric.width *
+            cabinBag.metric.height) /
+          1000
+        ).toFixed(1);
+
+        return {
+          slug,
+          personalItem,
+          cabinBag,
+          cabinBagVolume: parseFloat(cabinBagVolume),
+        };
+      })
+      .sort((a, b) => b.cabinBagVolume - a.cabinBagVolume);
+    setSortedBags(sortedBags);
+  }
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} bags={bags} />
+      <Component
+        {...pageProps}
+        bags={sortedBags}
+        onSortBagsAlphabetical={handleSortBagsAlphabetical}
+        onSortBagsPersonalItem={handleSortBagsPersonalItem}
+        onSortBagsCabinBag={handleSortBagsCabinBag}
+      />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
-import List from "./List";
+import List from "../components/List";
 
-export default function HomePage({ bags }) {
+export default function HomePage({
+  bags,
+  onSortBagsAlphabetical,
+  onSortBagsPersonalItem,
+  onSortBagsCabinBag,
+}) {
   return (
     <div>
       <h1>Hand luggage ninja</h1>
+      <button onClick={onSortBagsAlphabetical}>Alphabetical</button>
+      <button onClick={onSortBagsPersonalItem}>Personal item</button>
+      <button onClick={onSortBagsCabinBag}>Cabin bag</button>
+
       <List bags={bags} />
     </div>
   );
