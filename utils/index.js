fe2243a83684a1794067b39d13343b35bd31fa9e
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-07-17 17:26:16 +0200

    deleted an unnecessary helper function

--- utils/index.js
+++ utils/index.js
@@ -5,7 +5,3 @@ export function convertDimension(value, unitSystem) {
 export function calculateVolume({ length, width, height }) {
   return Math.round((length * width * height) / 1000);
 }
-
-export function calculateVolumeCombined(personalItem, cabinBag) {
-  return calculateVolume(personalItem) + calculateVolume(cabinBag);
-}
