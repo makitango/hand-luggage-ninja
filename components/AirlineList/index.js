09cf223f164148c26dd1ff898a188da99719d227
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-07-25 20:29:17 +0200

    adjust styling in AirlineList

--- components/AirlineList/index.js
+++ components/AirlineList/index.js
@@ -1,48 +1,105 @@
 import { calculateVolume, convertDimension } from "@/utils";
+import styled from "styled-components";
 
 export default function AirlineList({ airlines, unitSystem, bags }) {
   return (
-    <>
-      {airlines.map(({ id, name, personalItem, cabinBag, freeCabinBag }) => {
-        return (
-          <div key={id}>
-            <h2>{name}</h2>
-            <section>
-              <p>
-                <strong>Personal item </strong>
-                {convertDimension(personalItem.length, unitSystem)}
-                {" x "}
-                {convertDimension(personalItem.width, unitSystem)}
-                {" x "}
-                {convertDimension(personalItem.height, unitSystem)}
-                {unitSystem === "metric" ? " cm" : " in"}
-                {" | "}
-                <strong>{calculateVolume(personalItem)} l</strong>
-              </p>
-
-              <p>
-                <strong>Cabin bag </strong>
-                {convertDimension(cabinBag.length, unitSystem)}
-                {" x "}
-                {convertDimension(cabinBag.width, unitSystem)}
-                {" x "}
-                {convertDimension(cabinBag.height, unitSystem)}
-                {unitSystem === "metric" ? " cm" : " in"}
-                {" | "}
-                <strong>{calculateVolume(cabinBag)} l</strong>
-              </p>
-              <p>
-                <strong>
-                  Combined volume{" "}
-                  {calculateVolume(personalItem) + calculateVolume(cabinBag)}
-                  {" l"}
-                </strong>
-              </p>
-              {freeCabinBag ? "" : <p>💰 Cabin bag costs extra 💰</p>}
-            </section>
-          </div>
-        );
-      })}
-    </>
+    <CardContainer>
+      {airlines.map(({ id, name, personalItem, cabinBag, freeCabinBag }) => (
+        <Card key={id}>
+          <Name>{name}</Name>
+          <GridContainer>
+            <Column1>
+              <strong>Personal item</strong>
+            </Column1>
+            <Column2>
+              {convertDimension(personalItem.length, unitSystem)}
+              {" x "}
+              {convertDimension(personalItem.width, unitSystem)}
+              {" x "}
+              {convertDimension(personalItem.height, unitSystem)}
+              {unitSystem === "metric" ? " cm" : " in"}
+            </Column2>
+            <Column3>{calculateVolume(personalItem)} l</Column3>
+          </GridContainer>
+          <GridContainer>
+            <Column1>
+              <strong>Cabin bag</strong>
+            </Column1>
+            <Column2>
+              {convertDimension(cabinBag.length, unitSystem)}
+              {" x "}
+              {convertDimension(cabinBag.width, unitSystem)}
+              {" x "}
+              {convertDimension(cabinBag.height, unitSystem)}
+              {unitSystem === "metric" ? " cm" : " in"}
+            </Column2>
+            <Column3>{calculateVolume(cabinBag)} l</Column3>
+          </GridContainer>
+          <CombinedVolume>
+            <strong>Combined volume</strong>
+            {calculateVolume(personalItem) + calculateVolume(cabinBag)} l
+          </CombinedVolume>
+          {freeCabinBag ? (
+            ""
+          ) : (
+            <ExtraCost>💰 Cabin bag costs extra 💰</ExtraCost>
+          )}
+        </Card>
+      ))}
+    </CardContainer>
   );
 }
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+`;
+
+const Card = styled.div`
+  padding: 20px;
+  border: 1px solid #dddddd;
+  border-radius: 16px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+`;
+
+const Name = styled.h2`
+  text-align: left;
+  margin-top: 0;
+`;
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+  align-items: center;
+`;
+
+const Column1 = styled.div`
+  grid-column: 1;
+  text-align: left;
+  white-space: nowrap;
+`;
+
+const Column2 = styled.div`
+  grid-column: 2;
+  text-align: right;
+  white-space: nowrap;
+`;
+
+const Column3 = styled.div`
+  grid-column: 3;
+  text-align: right;
+  white-space: nowrap;
+`;
+
+const CombinedVolume = styled.div`
+  display: flex;
+  justify-content: space-between;
+  text-align: left;
+`;
+
+const ExtraCost = styled.p`
+  text-align: left;
+`;
